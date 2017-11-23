1fb755912a8501280b0df16f33c731aa21b9e49f
Sumaiya1431 <sumaiya.garag@gmail.com>
2017-11-23 18:22:13 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(name='gym_race',
       version='0.0.1',
-      install_requires=['gym>=0.2.3',
-                        'hfo_py>=0.2']
+      install_requires=['gym']  # And any other dependencies foo needs
+)  
